422a3666bd128a379291f7f4869cf38812cc409a
1grc <igrskzm0101@gmail.com>
2018-11-17 16:31:59 +0900

    sample

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <sample-component :title="indextitle">
+      <div class="come">
+        slot 1117
+      </div>
+    </sample-component>
+    <nuxt-link to="/about">ABOUT</nuxt-link>
+  </div>
+</template>
+
+<script>
+import SampleComponent from '~/components/SampleComponent.vue'
+
+export default {
+  components: {
+    SampleComponent
+  },
+  data() {
+    return {
+      indextitle: 'Nuxt.js data'
+    }
+  }
+}
+</script>
